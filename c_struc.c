738b32c3ef06ac1bb021a65691177754131fadd5
Adetoroye Oluleke <46929287+OluL@users.noreply.github.com>
2022-10-08 09:43:18 +0100

    learning c

---
+++ c_struc.c
@@ -0,0 +1,21 @@
+//Author: Adet Leke
+//Date: 10/10/2022
+/* Progamming 
+C*/
+#include <stdio.h>
+#include <conio.h>
+#define MAX 100
+
+int a = 50;
+
+void display();
+void main()
+{
+    printf("Hello \n");
+    display();
+}
+
+void display()
+{
+    printf("Leke's Lecture");
+}
---
+++ cl1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - learning main functions and structure in c
+*/
+
+int main()
+{
+    int age;
+    printf("Enter age: \n");
+    scanf("%d", &age);
+    printf("age is %d", age);
+    return 0;
+}
